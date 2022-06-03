53043ca51501963a0138ecd0726f71f539465725
PavelKazAn <frozzty@yandex.ru>
2022-06-03 19:14:01 +0300

    Добавил сервер Netty

---
+++ server/src/com/vorobev/netty/handler/CloudFileHandler.java
@@ -0,0 +1,37 @@
+package com.vorobev.netty.handler;
+
+
+import com.vorobev.cloud.CloudMessage;
+import com.vorobev.cloud.FileMessage;
+import com.vorobev.cloud.FileRequest;
+import com.vorobev.cloud.ListFiles;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CloudFileHandler extends SimpleChannelInboundHandler<CloudMessage> {
+
+    private Path currentDir;
+
+    public CloudFileHandler() {
+        currentDir = Path.of("server_files");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(new ListFiles(currentDir));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, CloudMessage cloudMessage) throws Exception {
+//        if (cloudMessage instanceof FileRequest fileRequest) {
+//            ctx.writeAndFlush(new FileMessage(currentDir.resolve(fileRequest.getName())));
+//        } else if (cloudMessage instanceof FileMessage fileMessage) {
+//            Files.write(currentDir.resolve(fileMessage.getName()), fileMessage.getData());
+//            ctx.writeAndFlush(new ListFiles(currentDir));
+//        }
+    }
+}
